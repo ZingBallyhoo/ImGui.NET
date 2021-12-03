52653cc09ca783eb4c6d9d319b18843db37d4915
zingballyhoo <zingballyhoo@live.co.uk>
2021-12-03 18:08:58 +0000

    add generation script
    (cherry picked from commit 781cd855fcc062ea4603898079fa463f310de807)

---
+++ do-generate.bat
@@ -0,0 +1,4 @@
+bin\Debug\CodeGenerator\net6.0\CodeGenerator.exe src\ImGui.NET\Generated cimgui
+bin\Debug\CodeGenerator\net6.0\CodeGenerator.exe src\ImPlot.NET\Generated cimplot
+bin\Debug\CodeGenerator\net6.0\CodeGenerator.exe src\ImGuizmo.NET\Generated cimguizmo
+bin\Debug\CodeGenerator\net6.0\CodeGenerator.exe src\ImNodes.NET\Generated cimnodes
