951f9378a37881dafc61d87606fd48e51c54b973
zingballyhoo <zingballyhoo@live.co.uk>
2023-08-14 00:03:43 +0100

    update generation script for .net7

--- do-generate.bat
+++ do-generate.bat
@@ -1,4 +1,4 @@
-bin\Debug\CodeGenerator\net6.0\CodeGenerator.exe src\ImGui.NET\Generated cimgui
-bin\Debug\CodeGenerator\net6.0\CodeGenerator.exe src\ImPlot.NET\Generated cimplot
-bin\Debug\CodeGenerator\net6.0\CodeGenerator.exe src\ImGuizmo.NET\Generated cimguizmo
-bin\Debug\CodeGenerator\net6.0\CodeGenerator.exe src\ImNodes.NET\Generated cimnodes
+bin\Debug\CodeGenerator\net7.0\CodeGenerator.exe src\ImGui.NET\Generated cimgui
+bin\Debug\CodeGenerator\net7.0\CodeGenerator.exe src\ImPlot.NET\Generated cimplot
+bin\Debug\CodeGenerator\net7.0\CodeGenerator.exe src\ImGuizmo.NET\Generated cimguizmo
+bin\Debug\CodeGenerator\net7.0\CodeGenerator.exe src\ImNodes.NET\Generated cimnodes
